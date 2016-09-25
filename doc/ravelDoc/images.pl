79e0f95ac2e6204b66d3ef34e67ec5aa1420f98e
Russell Standish <hpcoder@hpcoders.com.au>
2016-09-26 09:01:49 +1000

    Add "built" files to git repo, relating to documentation.

---
+++ doc/ravelDoc/images.pl
@@ -0,0 +1,12 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/cdots;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="23" HEIGHT="15" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$\cdots$">|; 
+
+1;
+
---
+++ doc/ravelDoc/internals.pl
@@ -0,0 +1,10 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/difficult/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
