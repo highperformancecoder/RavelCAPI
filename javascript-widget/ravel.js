21e14222bac3fc4434712e38d81df125ff75d571
Russell Standish <hpcoder@hpcoders.com.au>
2017-04-03 16:55:00 +1000

    added initialiseVector function

--- javascript-widget/ravel.js
+++ javascript-widget/ravel.js
@@ -175,9 +175,7 @@ function setTable(name,ravel) {
                 sliceLabelReq.onreadystatechange = function() {
                     if (this.readyState == 4 && this.status == 200) {
                         var sliceLabels=new Module.VectorString();
-                        var sliceLabelData=eval(this.responseText);
-                        for (var i=0; i<sliceLabelData.length; ++i)
-                            sliceLabels.push_back(sliceLabelData[i]);
+                        initialiseVector(sliceLabels, eval(this.responseText));
                         ravel.addHandle(this.axis,sliceLabels);
                         if (ravel.numHandles()==axes.length)
                         {
@@ -208,40 +206,9 @@ function setTable(name,ravel) {
     xhttp.send();
 }
 
-function addCommand(menu,label,cmd)
+// initialise a vector from a java array
+function initialiseVector(vec, arr)
 {
-    var entry=document.createElement("li");
-    menu.appendChild(entry);
-    entry.innerHTML=h.description;
-    entry.setAttribute("onclick",cmd);
-}
-
-function addMenu(menu,label)
-{
-    var entry=document.createElement("li");
-    menu.appendChild(entry);
-    var a=document.createElement("a");
-    entry.appendChild(a);
-    a.innerHTML=label;
-    a.setAttribute("href","#");
-    a.setAttribute("class","hsub");
-    var subMenu=document.createElement("ul");
-    entry.appendChild(subMenu);
-    return subMenu;
-}
-
-function axisMenu(ravel, menuId)
-{
-    var menu=document.getElementById(menuId);
-    while (menu.hasChildNodes())
-    {menu.removeChild(menu.firstChild);}
-    for (var i=0; i<ravel.numHandles(); ++i)
-    {
-        var h=ravel.handles(i);
-        var submenu=addMenu(menu,h.description);
-        addMenu(submenu,"sort");
-        addMenu(submenu,"reduce");
-        addMenu(submenu,"filter");
-        h.delete;
-    }
+    for (var i=0; i<arr.length; ++i)
+        vec.push_back(arr[i]);
 }
