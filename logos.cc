670226017a9654f635cd7ee24356ac43f7fc4ecf
Russell Standish <hpcoder@hpcoders.com.au>
2017-04-06 19:31:18 +1000

    Changed logo program to output 100x100 PNG file

--- logos.cc
+++ logos.cc
@@ -10,13 +10,14 @@ using namespace ecolab::cairo;
 int main()
 {
   RavelCairo<cairo_t*> rc;
-  rc.rescale(200);
+  rc.rescale(50);
   rc.addHandle("Year",{"1990","1991","1992"});
   rc.addHandle("Gender",{"Male","Female"});
   rc.addHandle("Country",{"Australia","UK","USA"});
 
-  Surface surf(cairo_svg_surface_create("logo.svg",500,500));
-  cairo_surface_set_device_offset(surf.surface(),250,250);
+  Surface surf(cairo_image_surface_create(CAIRO_FORMAT_ARGB32,100,100));
+  cairo_surface_set_device_offset(surf.surface(),50,50);
   rc.setG(surf.cairo());
   rc.render();
+  cairo_surface_write_to_png(surf.surface(),"logo100.png");
 }
