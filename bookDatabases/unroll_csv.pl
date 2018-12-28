b17238124463317f296496a267a301751aa1176c
Russell Standish <hpcoder@hpcoders.com.au>
2018-12-28 20:31:07 +1100

    update bookdatabases unroll tool

--- bookDatabases/unroll_csv.pl
+++ bookDatabases/unroll_csv.pl
@@ -2,13 +2,17 @@ use Text::CSV;
 $csv=Text::CSV->new({ binary => 1 } );
 
 # skip header
-for ($i=0; $i<6; ++$i) {$_=<>;}
+for ($i=0; $i<1; ++$i) {$_=<>;}
 chomp;
 $csv->parse($_);
 @colName=$csv->fields();
 
-$naxes=5;
-
+$naxes=9;
+for ($i=0; $i<$naxes; ++$i)
+{
+    print "$colName[$i],";
+}
+print "\$value\n";
 
 while (<>)
 {
@@ -22,7 +26,7 @@ while (<>)
         {
 #            if ($row[3] ne "A:All sectors" || $row[4] ne "M:Market value" ||
 #                $row[5] ne "USD:US Dollar" || $row[6] ne "A:Adjusted for breaks") {next;}
-            for ($j=1; $j<4; ++$j)
+            for ($j=1; $j<$naxes; ++$j)
             {
                 $row[$j]=~s/^.*://;
                 print "\"$row[$j]\",";
