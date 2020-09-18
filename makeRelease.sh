afab294e86072f9bf4f7e1780e83db90182bd207
Russell Standish <hpcoder@hpcoders.com.au>
2020-09-18 10:48:23 +1000

    Release 1.1.0-beta.86

--- makeRelease.sh
+++ makeRelease.sh
@@ -1,5 +1,8 @@
 echo "old version=`git describe`"
 echo '#define RAVEL_VERSION "'$1'"' >src/ravelVersion.h
+pushd RavelCAPI
+git tag -a -m "" $1
+git push
+popd
 git commit -a -m "Release $1"
 git tag -a -m "" $1
-
--- src/ravelVersion.h
+++ src/ravelVersion.h
@@ -1,1 +1,1 @@
-#define RAVEL_VERSION "1.1.0-beta.85"
+#define RAVEL_VERSION "1.1.0-beta.86"
