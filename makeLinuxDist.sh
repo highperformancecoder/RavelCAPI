d85bb10bd8d0f0ca81dc75c4b1f75dd569a280b0
Russell Standish <hpcoder@hpcoders.com.au>
2021-04-02 12:33:48 +1100

    Fixed up makeLinuxDist script.

--- makeLinuxDist.sh
+++ makeLinuxDist.sh
@@ -1,4 +1,17 @@
 version=`git describe`
 name=ravel-$version
+target=`pwd`/linux-distro/$name.tar
 
-git archive --format=tar.gz --prefix=$name/ HEAD -o linux-distro/$name.tar.gz
+git archive --format=tar --prefix=$name/ HEAD -o $target
+
+git submodule update --init --recursive
+git submodule | cut -f3 -d' '| while read module; do
+    pushd $module
+    git archive --format=tar --prefix=$name/$module/ HEAD -o /tmp/$$.tar
+    tar tvf /tmp/$$.tar
+    tar Avf $target /tmp/$$.tar
+    popd
+done
+
+gzip $target
+
