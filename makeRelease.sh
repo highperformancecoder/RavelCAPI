01e94023053e97c3919c7a16a300be0b623a2c9c
Russell Standish <hpcoder@hpcoders.com.au>
2021-04-03 15:53:52 +1100

    Release 1.1.0-beta.99

--- makeRelease.sh
+++ makeRelease.sh
@@ -8,8 +8,8 @@ git push
 popd
 pushd linux-distro
 semver=`tr '-' '~' $1`
-sed -e 's/^Version:.*\$/Version: $semver' -i ravel.spec
-sed -e 's/^Version:.*\$/Version: $semver-1' -i ravel.dsc
+sed -e 's/^Version:.*\$/Version: $semver/' -i ravel.spec
+sed -e 's/^Version:.*\$/Version: $semver-1/' -i ravel.dsc
 popd
 git commit -a -m "Release $1"
 git tag -a -m "" $1
