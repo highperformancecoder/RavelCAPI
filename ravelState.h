3df4103ccf44a9718eb06c54754f56d4764bcb53
Russell Standish <hpcoder@hpcoders.com.au>
2022-12-07 16:15:32 +1100

    Added a deprecated format field. Will be removed when building Ravel on Civita.

--- ravelState.h
+++ ravelState.h
@@ -39,6 +39,7 @@ namespace ravel
     bool collapsed=false, displayFilterCaliper=false;
     Op::ReductionOp reductionOp=Op::sum;
     HandleSort::Order order=HandleSort::none;
+    std::string format; // for deprecate time* sort orders
     std::vector<std::string> customOrder; 
     std::string minLabel, maxLabel, sliceLabel;
     HandleState() {}
