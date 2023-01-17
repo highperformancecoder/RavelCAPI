44e79789d29bb932adeb79e22c71ab6bfeeb8050
Russell Standish <hpcoder@hpcoders.com.au>
2023-01-18 09:44:08 +1100

    Civita CAPI stuff. For ravel #228.

--- capiCivita.h
+++ capiCivita.h
@@ -5,13 +5,19 @@
 #ifndef CAPICIVITA_H
 #define CAPICIVITA_H
 #include <stdlib.h>
+/// First argument of these method pointers is effectively the this pointer
 struct CAPITensor
 {
-  const char* (*hypercube)(struct  CAPITensor*);
-  size_t (*size)(struct  CAPITensor*);
-  size_t (*indexSize)(struct  CAPITensor*, size_t i);
-  size_t* (*index)(struct  CAPITensor*, size_t i);
-  double (*at)(struct  CAPITensor*,size_t i);
+  /// return a json representation of a hypercube
+  const char* (*hypercube)(const struct CAPITensor*);
+  /// return number of non-zero elements in the tensor
+  size_t (*size)(const struct CAPITensor*);
+  /// return number of elements in the index vector: 0 implies dense data 
+  size_t (*indexSize)(const struct CAPITensor*);
+  /// return \a index[i]
+  size_t (*index)(const struct CAPITensor*, size_t i);
+  /// return element \a i. If sparse, this is equivalent to T[index[i]].
+  double (*at)(const struct CAPITensor*,size_t i);
 };
 typedef struct CAPITensor CAPITensor;
 #endif
--- ravelCivita.h
+++ ravelCivita.h
@@ -2,50 +2,58 @@
   Ravel C API. © Ravelation Pty Ltd 2023
 */
 
-#ifndef CAPICIVITA_H
-#define CAPICIVITA_H
+#ifndef RAVELCIVITA_H
+#define RAVELCIVITA_H
 #include "capiCivita.h"
+#include "tensorInterface.h"
 
 /// Wrapper around Civita classes, providing CAPICivita interface
 
 namespace ravel
 {
   // wrap a tensor, implement a CAPITensor
-  struct CAPITensor: public ::CAPITensor
+  class CAPITensor: public ::CAPITensor
   {
-    const civita::ITensor& tensor;
-    std::string hypercube;
-    CAPITensor(const civita::ITensor& tensor): tensor(tensor)
+    const civita::ITensor& m_tensor;
+    static const civita::ITensor& tensor(const ::CAPITensor* x) {return static_cast<const CAPITensor*>(x)->m_tensor;}
+    std::string m_hypercube;
+    static const char* s_hypercube(const struct ::CAPITensor* x) {
+      auto& hc=const_cast<CAPITensor*>(static_cast<const CAPITensor*>(x))->m_hypercube;
+      hc=tensor(x).hypercube().json();
+      return hc.c_str();
+    }
+    static size_t s_size(const struct ::CAPITensor* x)
+    {return tensor(x).size();}
+    static size_t s_indexSize(const struct ::CAPITensor* x)
+    {return tensor(x).index().size();}
+    static size_t s_index(const struct ::CAPITensor* x, size_t i)
+    {return tensor(x).index()[i];}
+    static double s_at(const struct ::CAPITensor* x,size_t i)
+    {return tensor(x)[i];}
+  public:
+    CAPITensor(const civita::ITensor& tensor): m_tensor(tensor)
     {
       hypercube=s_hypercube;
       size=s_size;
+      indexSize=s_indexSize;
       index=s_index;
       at=s_at;
     }
-    static const char* s_hypercube(struct CAPITensor* x) {
-      x->hypercube=x->tensor.hypercube().json();
-      return x->hypercube.c_str();
-    }
-    static size_t s_size(struct  CAPITensor* x)
-    {return x->tensor.size();}
-    size_t (*index)(struct  CAPITensor* x, size_t i)
-    {return x->tensor.index()[i];}
-    double (*at)(struct  CAPITensor*,size_t i)
-    {return x->tensor[i];}
   };
 
   // wrap a CAPITensor, implement an ITensor
-  class TensorWrap: public ITensor
+  class TensorWrap: public civita::ITensor
   {
     const ::CAPITensor& tensor;
+  public:
     TensorWrap(const ::CAPITensor& tensor): tensor(tensor) {
-      std::set  idx;
-      for (size_t i=0; i<tensor.indexSize(); ++i)
-        idx.insert(tensor.index(i));
+      std::set<size_t> idx;
+      for (size_t i=0; i<tensor.indexSize(&tensor); ++i)
+        idx.insert(tensor.index(&tensor,i));
       m_index=idx;
-      hypercube(civita::fromJson(tensor.hypercube()));
+      hypercube(civita::Hypercube::fromJson(tensor.hypercube(&tensor)));
     }
-    double operator[](std::size_t i) const override {return tensor->at(i);}
+    double operator[](std::size_t i) const override {return tensor.at(&tensor,i);}
     Timestamp timestamp() const override {return {};}
   };
   
